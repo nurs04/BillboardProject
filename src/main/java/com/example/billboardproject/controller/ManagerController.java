5d50e3c068823ad76a12b3ae5d00b327b46988c1
Kozhan Nurassyl <nurasil21.07@mail.ru>
2023-04-11 23:25:11 +0600

    Preparing for second deploy 3.5

--- src/main/java/com/example/billboardproject/controller/ManagerController.java
+++ src/main/java/com/example/billboardproject/controller/ManagerController.java
@@ -137,7 +137,7 @@ public class ManagerController {
     public @ResponseBody byte[] getAva(@PathVariable(name = "token", required = false) String token) throws IOException {
         String pictureUrl = loadURL + "default.jpg";
         if(token != null) {
-            pictureUrl = loadURL;
+            pictureUrl = token;
 //            pictureUrl = loadURL + token + ".jpg";
         }
         InputStream in;
